8d7a48bd450dceca8ba056f4aac505dd4d76b432
Sug1re <sugirex@icloud.com>
2024-04-26 16:04:27 +0900

    resultテキストを追加

--- app/components/layouts/header/Header.tsx
+++ app/components/layouts/header/Header.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className="flex flex-col items-center justify-center h-[20vh] bg-gray-300 dark:bg-gray-800">
-      <h1 className="text-4xl font-bold text-blue-950 dark:text-blue-900">
+    <header className="flex flex-col items-center justify-center h-[14vh] bg-gray-900 ">
+      <h1 className="text-4xl font-bold text-blue-700 ">
         おてスピチェック
       </h1>
-      <div>
+      <div className='text-white'>
         Let’s check your Pokemon’s speed.
       </div>
     </header>
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Switch } from "@/components/ui/switch";
-import { Slider } from "@/components/ui/slider"
+import {CardFooter} from "@/components/ui/card";
 import {
   Select,
   SelectContent,
@@ -20,144 +10,143 @@ import {
 import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import { Sheet } from "lucide-react";
-import { SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 
-// export default function Home() {
+// function CustomCheckbox(props: { id: string }) {
 //   return (
-//     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-blue-300">
-//       <div className="bg-white rounded w-[450px]">
-//         <CardHeader className="items-center py-3">
-//           <CardTitle className="text-2xl font-bold text-blue-950">
-//             おてスピチェック
-//           </CardTitle>
-//           <CardDescription>
-//             {/* Let's check your Pokémon's speed. */}
-//             Let’s check your Pokemon’s speed.
-//           </CardDescription>
-//         </CardHeader>
-//         <CardContent>
-//           <form>
-//             <div className="flex flex-col space-y-1.5 px-8 py-2">
-//               <Label htmlFor="ポケモン">ポケモン</Label>
-//               <Input
-//                 id="name"
-//                 placeholder="Please input the Pokémon."
-//                 className="bg-white"
-//               />
-//             </div>
-//             <div className="flex flex-col space-y-1.5 px-8 py-2">
-//               <Label htmlFor="性格">性格</Label>
-//               <Input
-//                 id="name"
-//                 placeholder="Please input the Pokémon's personality."
-//                 className="bg-white"
-//               />
-//             </div>
-//             <div className="flex flex-col space-y-1.5 px-8 py-2">
-//               <Label htmlFor="framework">サブスキル</Label>
-//               <Select>
-//                 <SelectTrigger id="framework" className="bg-white">
-//                   <SelectValue placeholder="サブスキル" />
-//                 </SelectTrigger>
-//                 <SelectContent position="popper" className="bg-white">
-//                   <SelectItem value="S">おてつだいスピードS</SelectItem>
-//                   <SelectItem value="M">おてつだいスピードM</SelectItem>
-//                   <SelectItem value="M&S">両方</SelectItem>
-//                   <SelectItem value="なし">なし</SelectItem>
-//                 </SelectContent>
-//               </Select>
-//             </div>
-//             <div className="space-y-2">
-//               <div className="flex  space-x-2 px-8 pt-10 ml-20">
-//                 <Checkbox id="bonuss" className="rounded" />
-//                 <Label htmlFor="bonuss">おてつだいポーナス</Label>
-//               </div>
-//               <div className="flex  space-x-6 px-8 ml-20">
-//                 <Checkbox id="energy" className="rounded" />
-//                 <Label htmlFor="energy">げんき補正</Label>
-//               </div>
-//             </div>
-//             <div className="py-8">
-//               <Slider defaultValue={[33]} max={100} step={1} />
-//             </div>
-//           </form>
-//         </CardContent>
-//         <CardFooter className="flex justify-between px-8 py-5">
-//           <Button className=" bg-gray-200 rounded transform hover:bg-gray-100 hover:scale-95">
-//             Cancel
-//           </Button>
-//           <Button className=" bg-blue-950 text-white rounded transform hover:bg-blue-900 hover:scale-95">
-//             Check
-//           </Button>
-//         </CardFooter>
-//       </div>
-//     </main>
+//     <Checkbox
+//       {...props}
+//       className="rounded text-blue-700 bg-white border border-blue-700"
+//       // チェックボックスのアイコンの色を青にする
+//       iconClassName="text-blue-700"
+//     />
 //   );
 // }
 
-
 export default function Home() {
   return (
-    <main className="min-h-screen  items-center justify-between  bg-gray-100">
+    <main className="min-h-screen  items-center justify-between  bg-blue-950">
       <form>
-       <div className="grid lg:grid-cols-3 py-4 px-8 gap-4">
-        <div className="flex flex-col space-y-1.5">
-                  <Label className="text-2xl" htmlFor="ポケモン">ポケモン</Label>
-                  <Input
-                    id="name"
-                    placeholder="Please input the Pokémon."
-                    className="bg-white"
-                  />
+        <div className="grid lg:grid-cols-3 pt-4 px-24 gap-4">
+          <div className="space-y-1.5">
+                    <Label className="text-2xl text-blue-700" htmlFor="ポケモン">ポケモン</Label>
+                    <Input
+                      id="name"
+                      placeholder="Please input the Pokémon."
+                      className="bg-gray-400 text-white w-4/5"
+                    />
+          </div>
+          <div className="space-y-1.5">
+                    <Label className="text-2xl text-blue-700" htmlFor="性格">性格</Label>
+                    <Input
+                      id="name"
+                      placeholder="Please input the Pokémon’s personality."
+                      className="bg-gray-400 text-white w-4/5"
+                    />
+          </div>
+          <div className="space-y-1.5">
+                    <Label className="text-2xl text-blue-700" htmlFor="レベル">レベル</Label>
+                    <Input
+                      id="name"
+                      placeholder="Please tell me the level of the Pokémon."
+                      className="bg-gray-400 text-white w-4/5"
+                    />
+          </div>
+          <div className="space-y-1.5">
+                <Label className="text-2xl text-blue-700" htmlFor="framework">サブスキル</Label>
+                  <Select>
+                    <SelectTrigger id="framework" className="bg-gray-400 text-white w-4/5">
+                      <SelectValue placeholder="サブスキル" />
+                    </SelectTrigger>
+                      <SelectContent position="popper" className="bg-gray-400 text-white">
+                      <SelectItem value="S">おてつだいスピードS</SelectItem>
+                      <SelectItem value="M">おてつだいスピードM</SelectItem>
+                      <SelectItem value="M&S">両方</SelectItem>
+                      <SelectItem value="なし">なし</SelectItem>
+                    </SelectContent>
+                  </Select>
+          </div>
         </div>
-        <div className="flex flex-col space-y-1.5">
-                  <Label className="text-2xl" htmlFor="性格">性格</Label>
-                  <Input
-                    id="name"
-                    placeholder="Please input the Pokémon’s personality."
-                    className="bg-white"
-                  />
-        </div>
-        <div className="flex flex-col space-y-1.5">
-              <Label className="text-2xl" htmlFor="framework">サブスキル</Label>
-                <Select>
-                  <SelectTrigger id="framework" className="bg-white">
-                    <SelectValue placeholder="サブスキル" />
-                  </SelectTrigger>
-                    <SelectContent position="popper" className="bg-white">
-                    <SelectItem value="S">おてつだいスピードS</SelectItem>
-                    <SelectItem value="M">おてつだいスピードM</SelectItem>
-                    <SelectItem value="M&S">両方</SelectItem>
-                    <SelectItem value="なし">なし</SelectItem>
-                  </SelectContent>
-                </Select>
-        </div>
-      </div>
-        <div >
-          <Slider />
-        </div>
-        <div className="space-y-2">
-              <div className="  space-x-2 px-8 pt-10 ml-20">
-                <Checkbox id="bonuss" className="rounded" />
-                <Label htmlFor="bonuss" className="text-1xl">おてつだいボーナス</Label>
+        <div className="space-y-2 pr-48">
+              <div className="flex flex-row-reverse">
+                    <div className="flex flex-col space-y-1.5">
+                        <Label className="text-2xl text-blue-700 " htmlFor="framework">おてつだいボーナス</Label>
+                          <Select>
+                            <SelectTrigger id="framework" className="bg-gray-400 text-white ">
+                              <SelectValue placeholder="おてつだいボーナス" />
+                            </SelectTrigger>
+                              <SelectContent position="popper" className="bg-gray-400 text-white">
+                              <SelectItem value="1">1</SelectItem>
+                              <SelectItem value="2">2</SelectItem>
+                              <SelectItem value="3">3</SelectItem>
+                              <SelectItem value="4">4</SelectItem>
+                              <SelectItem value="5">5</SelectItem>
+                              <SelectItem value="0">0</SelectItem>
+                            </SelectContent>
+                          </Select>
+                    </div>
+                    <div className="space-x-4 px-8 pt-10 ml-20">
+                        <Checkbox
+                            id="bonuss"
+                            className="rounded text-white bg-gray-400"
+                        />
+                        <Label
+                            htmlFor="bonuss"
+                            className="text-1xl text-blue-700">
+                              おてつだいボーナス
+                        </Label>
+                    </div>
               </div>
-              <div className="space-x-6 px-8 ml-20">
-                <Checkbox id="energy" className="rounded" />
-                <Label htmlFor="energy" className="text-1xl">げんき補正</Label>
+              <div className="flex flex-row-reverse">
+                    <div className="flex flex-col space-y-1.5">
+                        <Label className="text-2xl text-blue-700 " htmlFor="framework">げんき補正</Label>
+                          <Select>
+                            <SelectTrigger id="framework" className="bg-gray-400 text-white w-52">
+                              <SelectValue placeholder="げんき補正" />
+                            </SelectTrigger>
+                              <SelectContent position="popper" className="bg-gray-400 text-white">
+                              <SelectItem value="80">80以上</SelectItem>
+                                  <SelectItem value="60">80未満60以上</SelectItem>
+                                  <SelectItem value="40">60未満40以上</SelectItem>
+                                  <SelectItem value="20">40未満20以上</SelectItem>
+                                  <SelectItem value="0">20未満</SelectItem>
+                            </SelectContent>
+                          </Select>
+                    </div>
+                    <div className="space-x-4 px-8 pt-10 ml-20">
+                        <Checkbox
+                            id="energy"
+                            className="rounded text-white bg-gray-400"
+                        />
+                        <Label
+                            htmlFor="energy"
+                            className="text-1xl text-blue-700">
+                              げんき補正
+                        </Label>
+                    </div>
               </div>
         </div>
-        <CardFooter className="flex justify-between px-14 py-5">
-          <Button className=" bg-gray-200 rounded transform hover:bg-white hover:scale-95">
-            Cancel
-          </Button>
-          <Button className=" bg-blue-950 text-white rounded transform hover:bg-blue-900 hover:scale-95">
-            Check
-          </Button>
-        </CardFooter>
-        <div className="px-8">
-            <Label htmlFor="result" className="text-2xl">Result</Label>
-            <Input id="result" readOnly type="text" value="0" className="bg-white"/>
+          <CardFooter className="flex flex-row justify-center items-center pt-10 pb-5 space-x-96">
+            <div className=" pr-48">
+                <Button className=" bg-gray-900  text-white rounded transform hover:bg-gray-800 hover:scale-95">
+                  Cancel
+                </Button>
+            </div>
+            <div className=" pl-48">
+                <Button className=" bg-blue-700 text-white rounded transform hover:bg-blue-600 hover:scale-95">
+                  Check
+                </Button>
+            </div>
+          </CardFooter>
+        <div className="px-8 flex justify-center items-center flex-col space-y-4">
+            <Label
+              htmlFor="result"
+              className="text-2xl text-blue-700">おてつだい時間</Label>
+            <Input
+              id="result"
+              readOnly type="text"
+              value="0"
+              className="bg-gray-400 text-white w-1/6 "
+            />
         </div>
       </form>
     </main>
