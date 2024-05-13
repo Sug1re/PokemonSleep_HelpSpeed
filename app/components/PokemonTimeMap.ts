be2a4cab41a1a90f53a36fab25db6ee8a2fcfe13
Sug1re <sugirex@icloud.com>
2024-05-13 15:26:06 +0900

    全ポケモン追加

--- app/components/PokemonTimeMap.ts
+++
@@ -1,7 +0,0 @@
-const pokemonTimeMap: { [pokemonName: string]: number } = {
-  "ピカチュウ": 2700,
-  "フシギダネ": 4400,
-  // 他のポケモン名と対応する時間を追加
-};
-
-export default pokemonTimeMap;
--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,146 @@ import { Button } from "@/components/ui/button";
 import { CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import Porsonality from "./components/Personality";
-import Level from "./components/Level";
 import Skill from "./components/Skill";
 import Bonuss from "./components/Bonuss";
 import Energy from "./components/Energy";
-import Pokemon from "./components/Pokemon";
 
 //ポケモンの固有のおてつだい時間を示す表
 const pokemonTimeMap: { [pokemonName: string]: number } = {
-  "ピカチュウ": 2700,
   "フシギダネ": 4400,
+  "フシギソウ": 3300,
+  "フシギバナ": 2800,
+  "ヒトカゲ": 3500,
+  "リザード": 3000,
+  "リザードン": 2400,
+  "ゼニガメ": 4500,
+  "カメール": 3400,
+  "カメックス": 2800,
+  "キャタピー": 4400,
+  "トランセル": 4200,
+  "バタフリー": 2600,
+  "コラッタ": 4900,
+  "ラッタ": 3200,
+  "アーボ": 5000,
+  "アーボック": 3700,
+  "ピカチュウ": 2700,
+  "ハロウィンピカチュウ": 2600,
+  "ホリデーピカチュウ": 2600,
+  "ライチュウ": 2200,
+  "ピッピ": 4000,
+  "ピクシー": 2800,
+  "ロコン": 4700,
+  "キュウコン": 2600,
+  "プリン": 3900,
+  "プクリン": 2900,
+  "ディグダ": 4300,
+  "ダグトリオ": 2800,
+  "ニャース": 4400,
+  "ペルシアン": 2800,
+  "コダック": 5400,
+  "ゴルダック:": 3400,
+  "マンキー": 4200,
+  "オコリザル": 2800,
+  "ガーディー": 4300,
+  "ウインディ": 2500,
+  "マダツボミ": 5200,
+  "ウツドン": 3800,
+  "ウツボット": 2800,
+  "イシツブテ": 5700,
+  "ゴローン": 4000,
+  "ゴローニャ": 3100,
+  "ヤドン": 5700,
+  "ヤドラン": 3800,
+  "コイル": 5800,
+  "レアコイル": 4000,
+  "ドードー": 3800,
+  "ドードーリオ": 2400,
+  "ゴース": 3800,
+  "ゴースト": 3000,
+  "ゲンガー": 2200,
+  "イワーク": 3100,
+  "カラカラ": 4800,
+  "ガラガラ": 3500,
+  "ガルーラ": 2800,
+  "バリヤード": 2800,
+  "カイロス": 2400,
+  "メタモン": 3500,
+  "イーブイ": 3700,
+  "シャワーズ": 3100,
+  "サンダース": 2200,
+  "ブースター": 2400,
+  "ミニリュウ": 5000,
+  "ハクリュウ": 3800,
+  "カイリュウ": 2600,
+  "チコリータ": 4400,
+  "ベイリーフ": 3300,
+  "メガニウム": 2800,
+  "ヒノアラシ": 3500,
+  "マグマラシ": 3000,
+  "バクフーン": 2400,
+  "ワニノコ": 4500,
+  "アリゲイツ": 3400,
+  "オオダイル": 2800,
+  "ピチュー": 4300,
+  "ピィ": 5600,
+  "ププリン": 5200,
+  "トゲピー": 4800,
+  "トゲチック": 3800,
+  "メリープ": 4600,
+  "モココ": 3300,
+  "デンリュウ": 2500,
+  "ウソッキー": 4000,
+  "エーフィー": 2400,
+  "ブラッキー": 3500,
+  "ヤドキング": 3700,
+  "ソーナンス": 3500,
+  "ハガネール": 3000,
+  "ヘラクロス": 2500,
+  "デリバード": 2500,
+  "デルビル": 4900,
+  "ヘルガー": 3300,
+  "ライコウ": 2100,
+   // "エンテイ":time[11][1],
+  // "スイクン":time[11][1],
+  "ヨーギラス": 4800,
+  "サナギラス": 3600,
+  "バンギラス": 2400,
+  "ラルトス": 4800,
+  "キルリア": 3500,
+  "サーナイト": 2400,
+  "ナマケロ": 4900,
+  "ヤルキモノ": 3500,
+  "ケッキング": 3800,
+  "ヤミラミ": 3600,
+  "ゴクリン": 5900,
+  "マルノーム": 3500,
+  "チルット": 4200,
+  "チルタリス": 3700,
+  "カゲボウズ": 3900,
+  "ジュペッタ": 2600,
+  "アブソル": 3100,
+  "ソーナノ": 5800,
+  "タマザラシ": 5600,
+  "トドクラー": 4000,
+  "トドゼルガ": 3000,
+  "ウソハチ": 6300,
+  "マネネ": 4300,
+  "リオル": 4200,
+  "ルカリオ": 2600,
+  "グレッグル": 5600,
+  "ドクロック": 3400,
+  "ユキカブリ": 5600,
+  "ユキノオー": 3000,
+  "ジバコイル": 3100,
+  "トゲキッス": 2600,
+  "リーフィア": 3000,
+  "グレイシア": 3500,
+  "エルレイド": 2400,
+  "ニンフィア": 2600,
+  "デデンネ": 2500,
+  "ヌイコグマ": 4100,
+  "キテルグマ": 2800,
+  "キュワワー": 2500,
   // 他のポケモン名と対応する時間を追加
 };
 
@@ -48,12 +177,13 @@ const pokemonPersonalityMap: { [pokemonPersonality: string]: number } = {
   "さみしがり": 0.9,
 };
 
+//サブスキルの倍率を示す表
+
 //使用する関数一覧
 export default function Home() {
   const [pokemonName, setPokemonName] = useState("");
   const [pokemonPersonality, setPokemonPersonality] = useState("");
   const [pokemonLevel, setPokemonLevel] = useState("");
-  // const [pokemonLevel, setPokemonLevel] = useState(0);
   const [pokemonSkill, setPokemonSkill] = useState("");
   const [pokemonBonuss, setPokemonBonuss] = useState("");
   const [pokemonEnergy, setPokemonEnergy] = useState("");
@@ -70,22 +200,29 @@ export default function Home() {
   };
 
   //ポケモンのレベルの入力フィールドの値が変更されたとき即座に対応する役割
-  const handlePokemonlevelChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPokemonLevel(event.target.value);
-  };
+const handlePokemonlevelChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const levelText = event.target.value;
+  // 文字列から整数に変換
+  const level = parseInt(levelText);
+  // NaN（非数値）チェック
+  if (!isNaN(level)) {
+    // レベルが1未満の場合、1に制限する
+    if (level < 1) {
+      setPokemonLevel("1");
+    } else {
+      // レベルが100を超える場合、100に制限する
+      if (level > 100) {
+        setPokemonLevel("100");
+      } else {
+        setPokemonLevel(level.toString());
+      }
+    }
+  } else {
+    // 数値に変換できない場合、空文字列にする
+    setPokemonLevel("");
+  }
+};
 
-  // const handlePokemonlevelChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   let level = parseInt(event.target.value);
-  //   // レベルが1未満の場合、1に制限する
-  //   if (level < 1) {
-  //     level = 1;
-  //   }
-  //   // レベルが100を超える場合、100に制限する
-  //   if (level > 100) {
-  //     level = 100;
-  //   }
-  //   setPokemonLevel(level);
-  // };
 
   //checkボタンをクリック時のフォームのリセット防止
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -101,10 +238,6 @@ export default function Home() {
       return; // ポケモンの性格が空の場合、何もしない
     }
 
-   // ポケモンのレベルが空でないかをチェック
-    // if (pokemonLevel === 0 || isNaN(pokemonLevel)) {
-    //   return; // ポケモンのレベルが空の場合、何もしない
-    // }
     if (pokemonLevel.trim() === "") {
       return; // ポケモンのレベルが空の場合、何もしない
     }
@@ -117,7 +250,6 @@ export default function Home() {
     setPokemonName("");
     setPokemonPersonality("");
     setPokemonLevel("");
-    // setPokemonLevel(0);
     // その他の状態を初期化する場合はここに追加
   };
 
@@ -131,14 +263,10 @@ const handleCheckButtonClick = () => {
   if (pokemonPersonality.trim() === "") {
     return; // ポケモンの性格が空の場合、何もしない
   }
-
   // ポケモンのレベルが空でないかをチェック
-    // if (pokemonLevel === 0 || isNaN(pokemonLevel)) {
-    //   return; // ポケモンのレベルが空の場合、何もしない
-    // }
-    if (pokemonLevel.trim() === "") {
-      return; // ポケモンのレベルが空の場合、何もしない
-    }
+  if (pokemonLevel.trim() === "") {
+    return; // ポケモンのレベルが空の場合、何もしない
+  }
 
   // ポケモンの名前と性格を数値に変換
   const pokemonTimeValue = pokemonTimeMap[pokemonName];
@@ -150,7 +278,6 @@ const handleCheckButtonClick = () => {
     const calculatedHelperTime = Math.floor(Math.floor(pokemonTimeValue *
                                                        pokemonPersonalityValue) *
                                                        (1.000 - (Math.floor( Number(pokemonLevel) - 1 )) * 0.002 ));
-                                                      //  (1.000 - (Math.floor( pokemonLevel - 1 )) * 0.002 ));
     setHelperTime(calculatedHelperTime);
   } else {
     // 存在しない場合はデフォルト値を設定
@@ -178,7 +305,7 @@ const handleCheckButtonClick = () => {
                     <Label className="text-2xl text-white" htmlFor="pokemonPesonality">性格</Label>
                     <Input
                       id="pokemonPersonality"
-                      type="txet"
+                      type="text"
                       placeholder="Please input the Pokémon’s personality."
                       className="bg-gray-400 text-white border-blue-700"
                       value={pokemonPersonality}
