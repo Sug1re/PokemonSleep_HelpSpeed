6868a0f8bef2fdeb9f5ba310220f45245e5760b8
yudai <sugi.nut@gmail.com>
2024-05-25 19:34:32 +0900

    cloud run用の設定ファイルを追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'standalone',
+    reactStrictMode: true,
+    swcMinify: true,
+  }
+  
+  module.exports = nextConfig;
