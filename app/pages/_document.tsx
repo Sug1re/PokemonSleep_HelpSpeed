f8179b89f2b7c1351d9af709df7b816f0d62c658
yudai <sugi.nut@gmail.com>
2024-06-09 17:23:20 +0900

    Headのscriptを修正 #8

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -5,10 +5,11 @@ export default function Document() {
   return (
     <Html lang="ja" prefix="og: http://ogp.me/ns#">
       <Head>
-        <script async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5584222931737430"
-        crossOrigin="anonymous">
-      </script>
+      <script
+          data-ad-client="ca-pub-5584222931737430"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        ></script>
       </Head>
       <body className='bg-gradient-to-r from-cyan-100 to-sky-100'>
         <Main />
