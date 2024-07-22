42f01a007af8da2bf983c1e52ba2e39777293e10
Sug1re <sugirex@icloud.com>
2024-07-22 15:01:00 +0900

    アイコン変更

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,20 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="ja" prefix="og: http://ogp.me/ns#">
       <Head>
-      <script
+        <link rel="icon" href="/favicon.ico" />
+        <script
           data-ad-client="ca-pub-5584222931737430"
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
         ></script>
       </Head>
-      <body className='bg-gradient-to-r from-cyan-100 to-sky-100'>
+      <body className="bg-gradient-to-r from-cyan-100 to-sky-100">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
