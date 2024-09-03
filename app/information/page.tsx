4180ef1722fe5b956db8a22fe963f57386e2effd
Sug1re <sugirex@icloud.com>
2024-09-03 15:58:09 +0900

    ＃スイクン追加

--- app/information/page.tsx
+++ app/information/page.tsx
@@ -85,6 +85,24 @@ const information = () => {
           </CardHeader>
         </Card>
       </div>
+      <div className=" p-4 ">
+        <Card className=" rounded sm:w-1/2 lg:w-1/3">
+          <CardHeader>
+            <div className=" flex justify-between mx-2 md:mx-4 ">
+              <div className=" flex flex-col">
+                <CardTitle>ポケモンの追加</CardTitle>
+                <div className="text-xs text-muted-foreground">
+                  <div>2024/09/3 : 「スイクン」を追加</div>
+                  {/* <div className="text-xs text-blue-400 hover:text-red-400 duration-300 flex justify-end">
+                    エンテイのステータスURL
+                  </div> */}
+                </div>
+              </div>
+              {/* <div className=" mx-2 bg-244-bg min-w-16 min-h-16 bg-cover bg-center object-cover"></div> */}
+            </div>
+          </CardHeader>
+        </Card>
+      </div>
       <div className="p-4">
         <h1>おてつだい時間に関する情報の出典</h1>
         <a
