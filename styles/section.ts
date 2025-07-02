def479f2f51ffa3c7e32b2ae24ddae6d23fc41d9
Sug1re <sugirex@icloud.com>
2025-07-02 14:55:27 +0900

    フォルダ構造を修正

---
+++ styles/section.ts
@@ -0,0 +1,17 @@
+export const buttonSx = {
+    textTransform: "none",
+    transition: "all 0.3s ease",
+    backgroundColor: "transparent",
+    ":hover": {
+      backgroundColor: "rgb(25, 61, 96)",
+      fontSize: "0.9rem",
+    },
+    boxShadow: "none",
+    all: "unset",
+    cursor: "pointer",
+    fontSize: "0.7rem",
+    flexGrow: 1,
+    display: "flex",
+    justifyContent: "center",
+    p: 1,
+  };
