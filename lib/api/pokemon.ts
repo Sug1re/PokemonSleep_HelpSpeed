b247bb565587511b802de35a0d1540d0f1f0c519
Sug1re <sugirex@icloud.com>
2025-07-10 11:36:08 +0900

    localURLを追加

--- lib/api/pokemon.ts
+++ lib/api/pokemon.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const baseURL = "https://pokemon-sleep-api-1059650888282.asia-northeast1.run.app";
+// const localBaseURL = "http://0.0.0.0:9090";
 
 export const fetchPokemonData = async () => {
   const res = await axios.get(`${baseURL}/pokemonName`);
+  // const res = await axios.get(`${localBaseURL}/pokemonName`);
   return res.data.pokemonData;
 };
 
@@ -14,5 +16,6 @@ export const calculatePokemonSpeed = async (formData: {
   level: number;
 }) => {
   const res = await axios.post(`${baseURL}/pokemonSpeed`, formData);
+  // const res = await axios.post(`${localBaseURL}/pokemonSpeed`, formData);
   return res.data;
 };
